c875d469634de95e44d1f33e6e53ef994f59bc43
MykytaPervushyn <80350205+MykytaPervushyn@users.noreply.github.com>
2021-11-08 00:23:03 +0200

    Updates

--- static/js/main.634eaed1.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-02-feedback"]=this["webpackJsonpgoit-react-hw-02-feedback"]||[]).push([[0],{11:function(e,t,c){"use strict";c.r(t);var n=c(1),r=c.n(n),i=c(3),s=c.n(i),a=(c(8),c.p,c(9),c(0));var o=function(){return Object(a.jsx)("div",{className:"App",children:Object(a.jsx)("h1",{children:"TEST"})})};s.a.render(Object(a.jsx)(r.a.StrictMode,{children:Object(a.jsx)(o,{})}),document.getElementById("root"))},8:function(e,t,c){},9:function(e,t,c){}},[[11,1,2]]]);
-//# sourceMappingURL=main.634eaed1.chunk.js.map
